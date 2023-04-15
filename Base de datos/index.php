0a9f9c01126bead9618db0d3195e4efff017f66e
MichellOsechas <90647320+MichellOsechas@users.noreply.github.com>
2023-04-14 20:45:57 -0500

    Add files via upload

---
+++ Base de datos/index.php
@@ -0,0 +1,19 @@
+<?php
+include'controller\conexion.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<?php
+include'controller\head.php';
+?>
+<body>
+    
+<?php
+include'view\formulario.php';
+?>
+</body>
+
+<?php
+include'controller\scripts.php';
+?>
+</html>
---
+++ Base de datos/view/formulario.php
@@ -0,0 +1,15 @@
+<form id="form">
+             
+        <input type="text" name="Nombre" placeholder="Ingrese su nombre" required><br>
+        <input type="text" name="Apellido" placeholder="Ingrese su apellido" required><br>
+        <input type="email" name="Correo" placeholder="Ingrese su Correo" required><br>
+
+        <label for="FechaDeNacimiento">Fecha de Nacimiento:</label>
+        <input type="date" name="FechaDeNacimiento" required><br>
+        
+        <textarea name="mensaje" cols="30" rows="10" placeholder="Ingrese su mensaje" required></textarea><br>
+
+        <input type="submit" value="Enviar" id="button">
+        
+    </form>
+    
